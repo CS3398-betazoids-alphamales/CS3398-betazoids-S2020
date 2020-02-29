85c8a838eb5b271435946dccbccd8027ccab2eb0
BenjaminWalkerBond <bwb63@txstate.edu>
2020-02-29 17:13:11 -0600

    "Search bar autocomplete and deleted old search in layout container"

---
+++ utility.js
@@ -0,0 +1,26 @@
+
+
+
+function getAllIngredients(searchText) {
+
+    var xhttp = new XMLHttpRequest();
+    
+
+    var targetUrl = 'https://us-central1-rvrslkupdb.cloudfunctions.net/getAllIngrs?' + searchText;
+    xhttp.open('POST', targetUrl);
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("getAll").innerHTML =
+          this.responseText;
+        }else if (this.status){
+            document.getElementById("getAll").innerHTML = "Can not access: " + targetUrl 
+                + " <br /> Response: " + this.status + " " + this.responseText;
+        }else{
+            document.getElementById("getAll").innerHTML = "Loading... ";
+        }
+      };
+      
+      
+      xhttp.send();
+    }
