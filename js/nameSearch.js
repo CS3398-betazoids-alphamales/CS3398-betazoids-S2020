501f1d4a58c6db7ab79e0d872873cc9803653ea2
jac541 <60242539+jac541@users.noreply.github.com>
2020-04-26 18:12:27 -0500

    mobile view edit

--- js/nameSearch.js
+++ js/nameSearch.js
@@ -17,6 +17,21 @@ $('#nav-search').on('keyup', function(event) {
   }
 })
 
+$('#nav-search2').on('keyup', function(event) {
+  var inner1 = document.getElementById("nav-search2").value;
+  console.log(inner1);
+ 
+  if(event.keyCode == 13){
+      document.getElementById("main-panel").innerHTML = `<div class="panel-head-wrapper" id="namehead"><h2>Search by Name:</h2></div><br><br>`;
+      if(inner1.split(" ").length == 1 || inner1.split(" ").length == 2 || inner1.split(" ").length == 3 || inner1.split(" ").length == 4 ){
+        console.log("The search query is: " + inner1);
+          lmNamePage = 1;
+          panelPurge();
+          getByName(inner1, lmNamePage);
+      }
+  }
+})
+
 function nameLoadMore(){
     var element = document.getElementById("lm-container");
     element.parentNode.removeChild(element);
